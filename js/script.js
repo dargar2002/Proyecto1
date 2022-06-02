7eeec0a63fe163135ace6a5c036681e9097fdba6
Dario <abnerdariogarcia@gmail.com>
2022-06-01 19:27:27 -0600

    firts commit

---
+++ js/script.js
@@ -0,0 +1,35 @@
+let contador = 0;
+
+const valor = document.querySelector('#valor');
+const botones = document.querySelectorAll('.boton')
+
+botones.forEach(boton => {
+  boton.addEventListener('click', function(e) {
+
+    const estilos = e.currentTarget.classList;
+
+    if(estilos.contains('disminuir')) {
+      contador--;
+    }
+    else if(estilos.contains('acrecentar')) {
+      contador++;
+    }
+    else {
+      contador = 0;
+    }
+      valor.textContent = contador;
+
+      // vamos a cambiar los colores 
+      if(contador == 0) {
+        valor.style.color = '#fff';
+      }
+
+      if(contador > 0) {
+        valor.style.color = '#03bb85';
+      }
+      if(contador < 0) {
+        valor.style.color = '#f80044';
+
+      }
+  })
+})
